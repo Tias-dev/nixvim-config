ebf71db3c4dadd64d529f21114e3cc5880385b0b
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-06-23 15:52:52 +0300

    disable clangd indexing

--- config/lsp/clangd.nix
+++ config/lsp/clangd.nix
@@ -1,4 +1,15 @@
 {pkgs, ...}: {
-  plugins.lsp.servers.clangd.enable = true;
+  plugins.lsp.servers.clangd = {
+    enable = true;
+    cmd = [
+      "clangd"
+      "--background-index=false"
+      "--clang-tidy"
+      "--header-insertion=iwyu"
+      "--completion-style=detailed"
+      "--function-arg-placeholders"
+      "--fallback-style=llvm"
+    ];
+  };
   extraPackages = [pkgs.clang-tools];
 }
