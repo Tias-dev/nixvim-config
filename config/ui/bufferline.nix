a1e3f2e92326d394e7e3ebdd835ee0cb006459c8
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-06-30 12:00:34 +0300

    add descriptions to hotkeys

--- config/ui/bufferline.nix
+++ config/ui/bufferline.nix
@@ -4,10 +4,10 @@
     settings = {};
   };
   keymaps = with keyLib; [
-    (base "<S-L>" "<cmd>BufferLineCycleNext<cr>")
-    (base "<S-H>" "<cmd>BufferLineCyclePrev<cr>")
-    (base "<leader>bo" "<cmd>BufferLineCloseOthers<cr>")
-    (base "<leader>bP" "<cmd>BufferLineGroupClose ungrouped<cr>")
-    (baseDesc "<leader>bd" "<cmd>bd<cr>" "delete current buffer")
+    (baseDesc "<S-L>" "<cmd>BufferLineCycleNext<cr>" "Pick next buffer")
+    (baseDesc "<S-H>" "<cmd>BufferLineCyclePrev<cr>" "Pick previous buffer")
+    (baseDesc "<leader>bo" "<cmd>BufferLineCloseOthers<cr>" "Delete other buffers")
+    (baseDesc "<leader>bP" "<cmd>BufferLineGroupClose ungrouped<cr>" "Delete unpinned buffers")
+    (baseDesc "<leader>bd" "<cmd>bd<cr>" "Delete current buffer")
   ];
 }
