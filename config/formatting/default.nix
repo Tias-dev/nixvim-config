e461bcb6459992e20f45c546bf912c3bcb449e30
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-07-06 15:25:50 +0300

    disable autopep8 experiment by default

--- config/formatting/default.nix
+++ config/formatting/default.nix
@@ -1,51 +1,66 @@
 {
   keyLib,
   pkgs,
+  lib,
+  config,
   ...
 }: {
-  plugins.conform-nvim = {
-    enable = true;
-    settings = {
-      formatters_by_ft = rec {
-        lua = ["stylua"];
-        cpp = ["clang_format"];
-        c = cpp;
-        python = ["isort" "black" "autopep8Experimental"];
-        nix = ["alejandra"];
-        latex = ["tex-fmt"];
-        "_" = [
-          "trim_whitespace"
-          "trim_newlines"
-        ];
-      };
-      format_on_save = {
-        lsp_format = "fallback";
-        timeout_ms = 500;
-      };
-      format_after_save = {
-        lsp_format = "fallback";
-      };
-      formatters = {
-        autopep8Experimental = {
-          "inherit" = false;
-          command = "autopep8";
-          args = ["--experimental" "$FILENAME"];
+  options = {
+    autopep8.experimental.enable = lib.mkEnableOption "experimental";
+  };
+  config = {
+    plugins.conform-nvim = {
+      enable = true;
+      settings = {
+        formatters_by_ft = rec {
+          lua = ["stylua"];
+          cpp = ["clang_format"];
+          c = cpp;
+          python = [
+            "isort"
+            "black"
+            (
+              if config.autopep8.experimental.enable
+              then "autopep8Experimental"
+              else "autopep8"
+            )
+          ];
+          nix = ["alejandra"];
+          latex = ["tex-fmt"];
+          "_" = [
+            "trim_whitespace"
+            "trim_newlines"
+          ];
+        };
+        format_on_save = {
+          lsp_format = "fallback";
+          timeout_ms = 500;
+        };
+        format_after_save = {
+          lsp_format = "fallback";
+        };
+        formatters = {
+          autopep8Experimental = {
+            "inherit" = false;
+            command = "autopep8";
+            args = ["--experimental" "$FILENAME"];
+          };
         };
       };
     };
+
+    keymaps = [
+      (keyLib.baseDesc "<leader>cf" ''<cmd>lua require("conform").format()<cr>'' "Format code")
+    ];
+
+    extraPackages = with pkgs; [
+      python3Packages.autopep8
+      stylua
+      clang-tools
+      isort
+      black
+      alejandra
+      tex-fmt
+    ];
   };
-
-  keymaps = [
-    (keyLib.baseDesc "<leader>cf" ''<cmd>lua require("conform").format()<cr>'' "Format code")
-  ];
-
-  extraPackages = with pkgs; [
-    python3Packages.autopep8
-    stylua
-    clang-tools
-    isort
-    black
-    alejandra
-    tex-fmt
-  ];
 }
