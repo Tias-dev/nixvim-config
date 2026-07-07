1cc2cf398040333d904cbca57f0a4662223ed207
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-07-07 19:48:49 +0300

    add tab keymaps and clues to them

--- config/editor/windows.nix
+++ config/editor/windows.nix
@@ -1,3 +1,8 @@
-{
+{keyLib, ...}: {
   plugins.tmux-navigator.enable = true;
+  keymaps = with keyLib; [
+    (baseDesc "<leader><Tab><Tab>" "<cmd>tabnew<cr>" "New Tab")
+    (baseDesc "<leader><Tab>d" "<cmd>tabclose<cr>" "Close current Tab")
+    (baseDesc "<leader><Tab>o" "<cmd>tabonly<cr>" "Close other Tabs")
+  ];
 }
