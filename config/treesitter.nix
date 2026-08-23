f1962e5c3b687947435e6e24c41a57d7d50ebfaf
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-08-23 17:29:52 +0300

    add limit 10 lines in treesitter-context

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,5 +1,10 @@
 {config, ...}: {
-  plugins.treesitter-context.enable = true;
+  plugins.treesitter-context = {
+    enable = true;
+    settings = {
+      max_lines = 10;
+    };
+  };
   plugins.treesitter = {
     enable = true;
     highlight.enable = true;
