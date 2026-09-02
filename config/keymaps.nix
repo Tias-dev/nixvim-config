1c70ac9bd9e220922438db2e52f8aee4cdc3848c
Timur Buchkin <timur.buchkin@mail.ru>
2026-09-02 21:52:38 +0300

    add ZZ and ZQ keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,5 +1,7 @@
 {keyLib, ...}: {
   keymaps = [
     (keyLib.baseDesc "<leader>pm" "<cmd>messages<cr>" "(misc) messages")
+    (keyLib.baseDesc "ZZ" "<cmd>wqa<cr>" "Quit (save before)")
+    (keyLib.baseDesc "ZQ" "<cmd>qa!<cr>" "Force quit (no save)")
   ];
 }
